edbb0c3e4274e6ad4123e70e6733c9809c475555
PatreceCampbell <patrece.campbell@mymona.uwi.edu>
2021-04-10 20:43:17 -0500

    Added flask form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class UploadForm(FlaskForm):
+
+    description = TextAreaField('Description',validators=[DataRequired()]) 
+    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
 
